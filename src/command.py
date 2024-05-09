9635df1f40307e1bd801147bbf0920487eda3eed
Ismail Karabulut <i.karabulut93@gmail.com>
2024-05-09 16:32:38 -0700

    add ability to add row

--- src/command.py
+++ src/command.py
@@ -14,8 +14,22 @@ class Command:
       exit()
     elif self.command == "print":
       self.print(data)
+    elif self.command == "add":
+      self.add(data, columns)
+    elif self.command == "sort":
+      self.sort(data, columns)
+
 
   def print(self, data):
     rows_to_print = input("Please enter how many rows you would like printed: \n")
     for row in data[:int(rows_to_print)]:
       print(*row)
+
+  def add(self, data, columns):
+    new_row = []
+    for col in columns:
+      col_info = input(f"Please enter the value for {col}: ")
+      new_row.append(col_info)
+    print("Row to be added: ")
+    print(*new_row)
+    data.append(new_row)
